ab05dfc335e7a9071b97c02fb9e39873a7d0d036
ilovelili <route666@live.cn>
2015-12-02 22:44:30 +0900

    array finder in golang

---
+++ duplicateFinder.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func panicOnError(err error) {
+	panic(err)
+}
+
+func getFirstDuplicate(elements []int) int {
+	// Use map to record duplicates since look up is important
+	encountered := map[int]bool{}
+
+	for v := range elements {
+		if encountered[elements[v]] == true {
+			return elements[v]
+		} else {
+			// Record this element as an encountered element.
+			encountered[elements[v]] = true
+		}
+	}
+	panicOnError(errors.New("No duplicate"))
+	return -1
+}
+
+func main() {
+	elements := []int{1, 2, 3, 1, 2, 4, 0}
+	fmt.Println("your array: ", elements)
+
+	// Test our method.
+	result := getFirstDuplicate(elements)
+	fmt.Println("first duplicate: ", result)
+}
